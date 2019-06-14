d7dff7745447634f78dbcb0c8d246f81f66a5837
Dakota Gallimore <gallimoredakota@gmail.com>
2019-06-14 00:01:47 -0400

    Add GraphQL Schema with Types to match JSON DB

---
+++ graphql-server/schema/schema.js
@@ -0,0 +1,42 @@
+const graphql = require('graphql');
+const axios = require('axios');
+
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt
+} = graphql;
+
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: {
+        id: { type: GraphQLInt },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString }
+    }
+});
+
+const PersonType = new GraphQLObjectType({
+    name: 'Person',
+    fields: {
+        id: { type: GraphQLInt },
+        firstName: { type: GraphQLString },
+        lastName: { type: GraphQLString },
+        profession: { type: GraphQLString }
+    }
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        person: {
+            type: PersonType,
+            args: { id: { type: GraphQLInt } },
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/persons/${args.id}`).then(
+                    resp => resp.data
+                )
+            }
+        }
+    }
+});
